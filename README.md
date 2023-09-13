e8fd6ddc7c314b6b559bab1c020ab0212f2c06c2
Rick Dennison <rdennison12@gmail.com>
2023-09-13 11:26:50 -0400

    Add bootstrap and jQuery

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ play solution to get a users local timezone.
 ## Commits
 * Initial Commit - Project folder created
 * Added entry point e.g. index.php
+* Add bootstrap and jQuery
--- index.php
+++ index.php
@@ -8,12 +8,33 @@ $serverTimeZone = date_default_timezone_get();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>TimeZone Testing</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
 </head>
 <body>
 
+<div class="container">
     <h1>Server Timezone</h1>
-    <p><?= $serverTimeZone?></p>
+    <p><?= $serverTimeZone ?></p>
+
+
+    <script src="js/jquery-3.7.1.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
+            crossorigin="anonymous"></script>
+    <!--<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js"
+            integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa"
+            crossorigin="anonymous"></script>-->
+
+
+</div>
 
 </body>
 </html>
