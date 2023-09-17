37f034c68855aed03accbe4fff55fe5edd9dd3d3
Rick Dennison <rdennison12@gmail.com>
2023-09-17 12:18:00 -0400

    Moved Repo to external storage

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ play solution to get a users local timezone.
 * Initial Commit - Project folder created
 * Added entry point e.g. index.php
 * Add bootstrap and jQuery
+* Moved Repo to external storage
--- index.php
+++ index.php
@@ -2,6 +2,16 @@
 
 $serverTimeZone = date_default_timezone_get();
 
+$serverTime = date("D, d-m-y H:i");
+
+$timezone_abbreviations = DateTimeZone::listAbbreviations();
+//print_r($timezone_abbreviations["acst"]);
+
+//var_dump($timezone_abbreviations["America"]);
+$timezone_identifiers = DateTimeZone::listIdentifiers();
+//for ($i=0; $i < 5; $i++) {
+//    echo "$timezone_identifiers[$i]\n";
+//}
 ?>
 
 <!DOCTYPE html>
@@ -17,9 +27,13 @@ $serverTimeZone = date_default_timezone_get();
 <body>
 
 <div class="container">
-    <h1>Server Timezone</h1>
+    <h3>Server Timezone</h3>
     <p><?= $serverTimeZone ?></p>
+    <h3>Server Time</h3>
+    <p><?= $serverTime ?></p>
 
+    <h3>Computer Time</h3>
+    <p id="computerTime"></p>
 
     <script src="js/jquery-3.7.1.min.js"></script>
     <script src="js/jquery-ui.min.js"></script>
@@ -32,7 +46,25 @@ $serverTimeZone = date_default_timezone_get();
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js"
             integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa"
             crossorigin="anonymous"></script>-->
+    <script type="text/javascript">
+        var currentTime = new Date()
+        var hours = currentTime.getHours()
+        var minutes = currentTime.getMinutes()
 
+        // if (minutes < 10){
+        //     minutes = "0" + minutes;
+        // }
+        // // var suffix = "AM";
+        // if (hours >= 12) {
+        //     // suffix = "PM";
+        //     hours = hours - 12;
+        // }
+        // if (hours === 0) {
+        //     hours = 12;
+        // }
+
+        document.write("<b>" + hours + ":" + minutes + " " +"</b>");
+    </script>
 
 </div>
 
